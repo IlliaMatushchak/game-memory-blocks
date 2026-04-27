404c98947c2c0e331bf3f2bd4fb40d58c077e174
IlliaMatushchak <coldnight100@gmail.com>
2026-04-27 15:08:06 +0300

    Feature: Improve appearance & Add the ability to customize settings

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
-let size = { rows: 3, cols: 3 };
-let appearanceDuration = 1;
-let answerDuration = 4;
-let boardContainerEl = document.querySelector("#board-container");
-let btnStart = document.querySelector("#btn-start");
+const boardContainerEl = document.querySelector("#board-container");
 const scoreEl = document.querySelector("#score");
+const btnStart = document.querySelector("#btn-start");
+
+const settingsEl = document.querySelector(".settings");
+const btnShowSettings = document.querySelector("#btn-show-settings");
+const btnCloseSettings = document.querySelector("#btn-close-settings");
+const formSettings = document.querySelector("#form-settings");
+
+const rowsInput = document.getElementById("rows");
+const colsInput = document.getElementById("cols");
+const appearanceInput = document.getElementById("appearance");
+const answerInput = document.getElementById("answer");
+
+let size = {};
+size.rows = Number(rowsInput.value);
+size.cols = Number(colsInput.value);
+let appearanceDuration = Number(appearanceInput.value);
+let answerDuration = Number(answerInput.value);
+
+btnShowSettings.addEventListener("click", () => {
+  settingsEl.classList.add("show");
+});
+
+btnCloseSettings.addEventListener("click", () => {
+  settingsEl.classList.remove("show");
+});
+
+formSettings.addEventListener("submit", (e) => {
+  e.preventDefault();
+  size.rows = Number(rowsInput.value);
+  size.cols = Number(colsInput.value);
+  appearanceDuration = Number(appearanceInput.value);
+  answerDuration = Number(answerInput.value);
+});
 
 btnStart.addEventListener("click", startGame);
 function startGame() {
